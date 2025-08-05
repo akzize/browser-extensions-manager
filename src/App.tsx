0e64fd44a55fdecbe888858186c695de7b8a13e1
akzize <akziz2zakaria@gmail.com>
2025-08-05 12:40:41 +0100

    refactor App component and enhance styling for extensions list

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react';
 import './App.css'
+import extensions from "./data/data.json";
+import image from "./assets/images/logo.svg"
+import DarkModeImg from "./assets/images/icon-moon.svg"
+import LightModeImg from "./assets/images/icon-sun.svg"
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    console.log(extensions);
 
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <header>
+          <div className="log">
+            <img src={image} alt="" />
+          </div>
+          <div className="theme-switcher">
+            <button>
+              <img src={DarkModeImg} alt="" />
+            </button>
+          </div>
+        </header>
+
+        {/* Extensions filter */}
+        <div className="filter">
+          <h1>Extension List</h1>
+          <div className="filters">
+            <button>All</button><button>Active</button><button>Inactive</button>
+          </div>
+        </div>
+        {/* Extensions list */}
+        <div className="extensions">
+          {
+            extensions.map(({ logo, name, description }, index) => {
+              const toggleId = "toggle-switch-" + index
+              return (
+
+                <div className="extension" key={index}>
+                  <div className="top">
+                    <div className="logo">
+                      <img src={logo} alt="Logo" />
+                    </div>
+                    <div className="txt">
+                      <h1>{name}</h1>
+                      <p>{description}</p>
+                    </div>
+                  </div>
+                  <div className="actions">
+                    <button>Remove</button>
+                    <label htmlFor={toggleId} className='switch'>
+                      <input type="checkbox" id={toggleId} />
+                      <span className='slider'></span>
+                    </label>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+          Coded by <a href="#">Zakaria Akziz</a>.
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
