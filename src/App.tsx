ae6f539389366894219992a520e41cc93aacde62
akzize <akziz2zakaria@gmail.com>
2025-08-09 12:10:45 +0100

    refactor App component to improve extension management and filtering functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,65 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
-import extensions from "./data/data.json";
+import data from "./data/data.json";
 import image from "./assets/images/logo.svg"
 import DarkModeImg from "./assets/images/icon-moon.svg"
 import LightModeImg from "./assets/images/icon-sun.svg"
+import { Extension } from './Extension.jsx';
+
+interface Extension {
+  id: number;
+  logo: string;
+  name: string;
+  description: string;
+  isActive: boolean;
+}
 
 function App() {
+  const [extensions, setExtensions] = useState([] as Extension[]);
+  // const [filteredExtensions, setFilteredExtensions] = useState([] as Extension[]);
+  const [selectedFilter, setSelectedFilter] = useState("all");
   useEffect(() => {
-    console.log(extensions);
+    let id = 1;
+    const extensionsList = data.map((extension) => {
+      return { id: id++, ...extension, isActive: false }
+    })
 
+    setExtensions(extensionsList)
+    // setFilteredExtensions(extensionsList);
+    console.log(extensionsList);
   }, [])
+
+  // Derived value for filtered list
+  const filteredExtensions = extensions.filter(ext => {
+    if (selectedFilter === "active") return ext.isActive;
+    if (selectedFilter === "inactive") return !ext.isActive;
+    return true; // "all"
+  });
+
+  // handle extension remove
+  function removeExtension(id: number) {
+    const filteredExtensionList = extensions.filter((extension) => extension.id != id)
+    setExtensions(filteredExtensionList);
+    // setFilteredExtensions(filteredExtensionList)
+  }
+
+  function toggleExtension(id: number) {
+    setExtensions(extensions.map((extension) => {
+      return extension.id === id ? { ...extension, isActive: !extension.isActive } : extension
+    }))
+  }
+
+  function toggleFilter(filter: string) {
+    // let filteredList = extensions;
+    // if (filter == "active") {
+    //   filteredList = filteredList.filter((extension) => extension.isActive)
+    // } else if (filter == "inactive") {
+    //   filteredList = filteredList.filter((extension) => extension.isActive == false)
+    // }
+
+    setSelectedFilter(filter);
+    // setFilteredExtensions(filteredList)
+  }
   return (
     <>
       <div className="container">
@@ -28,16 +78,17 @@ function App() {
         <div className="filter">
           <h1>Extension List</h1>
           <div className="filters">
-            <button>All</button><button>Active</button><button>Inactive</button>
+            <button className={selectedFilter == "all" ? "active" : ""} onClick={() => toggleFilter("all")}>All</button>
+            <button className={selectedFilter == "active" ? "active" : ""} onClick={() => toggleFilter("active")}>Active</button>
+            <button className={selectedFilter == "inactive" ? "active" : ""} onClick={() => toggleFilter("inactive")}>Inactive</button>
           </div>
         </div>
         {/* Extensions list */}
         <div className="extensions">
           {
-            extensions.map(({ logo, name, description }, index) => {
-              const toggleId = "toggle-switch-" + index
+            filteredExtensions.map(({ id, logo, name, description, isActive }, index) => {
+              const toggleId = "toggle-switch-" + index;
               return (
-
                 <div className="extension" key={index}>
                   <div className="top">
                     <div className="logo">
@@ -49,21 +100,22 @@ function App() {
                     </div>
                   </div>
                   <div className="actions">
-                    <button>Remove</button>
+                    <button onClick={() => removeExtension(id)}>Remove</button>
                     <label htmlFor={toggleId} className='switch'>
-                      <input type="checkbox" id={toggleId} />
+                      <input type="checkbox" id={toggleId} checked={isActive} onChange={() => toggleExtension(id)} />
                       <span className='slider'></span>
                     </label>
                   </div>
                 </div>
               )
+
             })
           }
         </div>
 
         <div className="attribution">
           Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
-          Coded by <a href="#">Zakaria Akziz</a>.
+          Coded by <a href="https://github.com/akzize">Zakaria Akziz</a>.
         </div>
       </div>
     </>
